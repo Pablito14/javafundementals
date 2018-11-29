e0f9e9e279b6e7c7198a7a4e7347a00903b5076d
pablo <pablo.rosales14@gmail.com>
2018-11-29 13:25:19 -0800

    got the first one done, just need to test it now... that is at least a third of todays lab done

--- basiclibrary/src/test/java/LibraryTest.java
+++ basiclibrary/src/test/java/LibraryTest.java
@@ -9,4 +9,22 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+
+    @Test public void testForDuplicates(){
+        int[] arrayToCheckF = {0, 1, 2};
+        int[] arrayToCheckT = {1, 1, 1};
+        assertFalse("Testing duplicate method for duplicates", Library.containsDuplicates(arrayToCheckF));
+        assertTrue("Testing duplicate method for duplicates", Library.containsDuplicates(arrayToCheckT));
+    }
+
+    @Test public void testAnalyzeWeatherData(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        assert
+    }
+
 }
