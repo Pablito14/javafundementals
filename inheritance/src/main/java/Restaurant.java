0ab42093413417b15db6595e2c9b23551b69b4e0
pablo <pablo.rosales14@gmail.com>
2018-12-04 13:35:37 -0800

    Got as far as I could this morning, will attempt to complete still.

--- inheritance/src/main/java/Restaurant.java
+++ inheritance/src/main/java/Restaurant.java
@@ -1,19 +1,37 @@
 import java.util.ArrayList;
-
-public class Restaurant extends Review {
+//Restaurant is the super class so Review gets Restaurant
+public class Restaurant /*extends Review */{
 
     private String name;
     private double stars;
     private int priceAverage;
     private ArrayList reviews;
 
+    /*Empty Restaurant Object*/
+    public Restaurant(){
+
+    }
+
+    /*Semi Initialized Restaurant (no review(s) upon creation)*/
     public Restaurant(String name, double stars, int priceAverage) {
         this.name = name;
         this.stars = stars;
         this.priceAverage = priceAverage;
-        this.reviews = new ArrayList<>();
+        this.reviews = new ArrayList<Review>();
     }
 
+    /*Fully Initialized Restaurant (with review(s) upon creation)*/
+    public Restaurant(String name, double stars, int priceAverage, Review reviewOnThisLocation) {
+        this.name = name;
+        this.stars = stars;
+        this.priceAverage = priceAverage;
+        this.reviews = new ArrayList<Review>();
+        reviews.add(reviewOnThisLocation);
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
     public String toString(){
         return ("name: " + this.name + ", stars: " + this.stars + ", price: " + this.priceAverage);
--- inheritance/src/main/java/Review.java
+++ inheritance/src/main/java/Review.java
@@ -1,27 +1,38 @@
-public class Review {
+public class Review extends Restaurant{
 
+    /*Instance Variables*/
     private String body;
     private String author;
     private double stars;
-    private String location;
+    private String reviewedLocation;
 
+    /*Empty Review Object */
+    public Review(){
+
+    }
+
+    /*Semi Initialized Review Object (no Location )*/
+//    public Review(String body, String author, double stars){
+//        this.body = body;
+//        this.author = author;
+//        this.stars = stars;
+//        this.reviewedLocation = null;
+//    }
+
+    /*Fully Instantiated Review Object*/
     public Review(String body, String author, double stars){
         this.body = body;
         this.author = author;
         this.stars = stars;
+        this.reviewedLocation = super.getName();
     }
-
-    public Review(String body, String author, double stars, String location){
-        this.body = body;
-        this.author = author;
-        this.stars = stars;
-        this.location = location;
-    }
-
-
 
     public String toString(){
-        return ("body: \"" + this.body + "\", author: " + this.author + ", stars: " + this.stars);
+        if(this.reviewedLocation != null) {
+            return ("body: \"" + this.body + "\", author: " + this.author + ", stars: " + this.stars + ", reviewedLocation: " + reviewedLocation);
+        } else {
+            return ("body: \"" + this.body + "\", author: " + this.author + ", stars: " + this.stars);
+        }
     }
 
 }
