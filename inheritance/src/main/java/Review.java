7bca34d80ea6af4da14a7376c8ca8e89aa36c51e
pablo <pablo.rosales14@gmail.com>
2018-12-09 23:48:04 -0800

    trying to access all the reviews stars properties is problematic

--- inheritance/src/main/java/Review.java
+++ inheritance/src/main/java/Review.java
@@ -1,12 +1,25 @@
-import java.util.*;
+public class Review {
+    private String body;
+    private int stars;
+    private String author;
+    protected Restaurant restaurant;
 
-public interface Review{
+    public String getBody() {return body;}
 
-    public String getBody();
-    public String getAuthor();
-    public double getStars();
-    public String toString();
+    public int getStars() {return stars;}
+
+    public String getAuthor() {return author;}
+
+    public Review(String body, int stars, String author){
+        this.body = body;
+        this.stars = stars;
+        this.author = author;
+        this.restaurant = null;
+    }
+
+    @Override
+    public String toString(){
+        return "Body: " + body + ", Stars: " + stars + ", Author: " + author;
+    }
 
 }
-
-
